c51af811475ad7475a9c14ae750eed799a0d3a49
Togepi <johannvdt@gmail.com>
2026-08-26 09:18:37 +0200

    Changement du better auth url

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -7,6 +7,7 @@ import { db } from '$lib/server/db';
 
 export const auth = betterAuth({
 	secret: env.BETTER_AUTH_SECRET,
+	baseURL: env.BETTER_AUTH_URL,
 	database: drizzleAdapter(db, { provider: 'pg' }),
 	emailAndPassword: { enabled: true },
 	plugins: [
